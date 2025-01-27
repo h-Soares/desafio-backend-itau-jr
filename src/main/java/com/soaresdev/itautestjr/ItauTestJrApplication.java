dfc33c54119f7d10b625b98752a34b3e6fdb1fbb
h-Soares <hiagosoares002@gmail.com>
2025-01-27 14:17:54 -0300

    Initial commit

---
+++ src/main/java/com/soaresdev/itautestjr/ItauTestJrApplication.java
@@ -0,0 +1,13 @@
+package com.soaresdev.itautestjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauTestJrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItauTestJrApplication.class, args);
+    }
+
+}
