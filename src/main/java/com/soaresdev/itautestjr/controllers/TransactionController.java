c7bae687ae7f6cb889fb214bfc5023500fa5cd9e
h-Soares <hiagosoares002@gmail.com>
2025-01-29 00:12:04 -0300

    hotfix: removed GET statistics from transaction controller.

--- src/main/java/com/soaresdev/itautestjr/controllers/TransactionController.java
+++ src/main/java/com/soaresdev/itautestjr/controllers/TransactionController.java
@@ -1,10 +1,8 @@
 package com.soaresdev.itautestjr.controllers;
 
-import com.soaresdev.itautestjr.dtos.StatisticsDto;
 import com.soaresdev.itautestjr.dtos.TransactionDto;
 import com.soaresdev.itautestjr.services.TransactionService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -42,16 +40,5 @@ public class TransactionController {
     public ResponseEntity<Void> deleteTransaction() {
         transactionService.deleteAllTransactions();
         return ResponseEntity.ok().build();
-    }
-
-    @Operation(description = "Get a summary of transactions that occurred in the last specified seconds", method = "GET")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content()),
-            @ApiResponse(responseCode = "422", description = "Unprocessable entity", content = @Content())
-    })
-    @GetMapping
-    public ResponseEntity<StatisticsDto> calculateStatistics(@RequestParam(required = false, defaultValue = "60") Long seconds) {
-        return ResponseEntity.ok(transactionService.calculateStatistics(seconds));
     }
 }
